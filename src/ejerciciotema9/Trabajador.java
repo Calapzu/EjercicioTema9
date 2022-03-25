f1c68082f38011ab037f7b511190383d33697515
Calapzu <yamsoncalapzu@gmail.com>
2022-03-25 15:29:42 -0500

    Se crea la clase Trabajador y se le agrega lineas al archivo .gitignore

---
+++ src/ejerciciotema9/Trabajador.java
@@ -0,0 +1,21 @@
+package ejerciciotema9;
+
+public class Trabajador extends Persona{
+    double salario;
+
+    public Trabajador() {
+    }
+
+    public Trabajador(int edad, String nombre, String telefono, double salario) {
+        super(edad, nombre, telefono);
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
